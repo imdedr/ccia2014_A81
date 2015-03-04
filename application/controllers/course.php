29566b058338c045113e1f88df948f16a7c21b0e
Keniver <keniver@keniver.com>
2015-03-05 01:52:46 +0800

    Hand up

--- application/controllers/course.php
+++ application/controllers/course.php
@@ -251,6 +251,196 @@ class Course extends CI_Controller {
 		
 	}
 
+	public function handup( $cid=0 ) {
+
+		$cid = (int)$cid;
+		$data = array( 'rcode' => 500, 'msg' => '' );
+
+		$token = $this->input->post('token');
+
+		if( $cid != 0 ) {
+
+			if( $token != null ) {
+
+				$this->load->model('token');
+				$user = $this->token->tokenAuth( $token );
+
+				if( $user != false ) {
+
+					$this->db->insert( 'handup', array( 'cid'=>$cid, 'uid'=>$user->uid, 'token'=>'', 'time'=>time() ) );
+
+					$data['rcode'] = 200;
+
+				} else {
+					$data['msg'] = 'Unavaliable Token.';	
+				}
+
+			} else {
+				$data['msg'] = 'Token is missing.';
+			}
+			
+		} else {
+			$data['msg'] = 'Missing parameter.';
+		}
+
+		echo json_encode($data);
+
+	}
+
+	public function handcheck( $cid=0 ) {
+		$cid = (int)$cid;
+		$data = array( 'rcode' => 500, 'msg' => '' );
+
+		$token = $this->input->post('token');
+
+		if( $cid != 0 ) {
+
+			if( $token != null ) {
+
+				$this->load->model('token');
+				$user = $this->token->tokenAuth( $token );
+
+				if( $user != false ) {
+
+					// 取得舉手狀態
+					$hand = $this->db->get_where( 'handup', array( 'cid'=>$cid, 'uid'=>$user->uid ) );
+					if( $hand->num_rows == 1 ) {
+						
+						$hand = $hand->result()[0];
+						if( $hand->token != '' ) {
+							// 老師允許舉手
+							$data['rcode'] = 200;
+							$data['handtoken'] = $hand->token;	
+						} else {
+							// 老師沒回應 300
+							$data['rcode'] = 300;
+							$data['msg'] = 'wait';
+						}
+
+					} else {
+						// 老師可能拒絕舉手
+						$data['rcode'] = 404;
+						$data['msg'] = 'No Hand';
+					}
+
+				} else {
+					$data['msg'] = 'Unavaliable Token.';	
+				}
+
+			} else {
+				$data['msg'] = 'Token is missing.';
+			}
+			
+		} else {
+			$data['msg'] = 'Missing parameter.';
+		}
+
+		echo json_encode($data);
+	}
+
+	public function hand_command( $cid=0 ) {
+
+		$cid = (int)$cid;
+		$data = array( 'rcode' => 500, 'msg' => '' );
+
+		$token = $this->input->post('token');
+		$hand_token = $this->input->post('hand_token');
+		$command = $this->input->post('command');
+
+		if( $hand_token == NULL || $hand_token == '' ) {
+			$data['msg'] = 'Missing Hand Token';
+			echo json_encode($data);
+			return;
+		}
+
+		if( $command == NULL || $command == '' ) {
+			$data['msg'] = 'No Command';
+			echo json_encode($data);
+			return;
+		}
+
+
+		if( $cid != 0 ) {
+
+			if( $token != null ) {
+
+				$this->load->model('token');
+				$user = $this->token->tokenAuth( $token );
+
+				if( $user != false ) {
+
+					// 將資料寫入	Queue
+					$this->db->insert( 'hand_queue', array( 'cid'=>$cid, 'token'=>$hand_token, 'time'=>time(), 'command'=>$command ) );
+					$data['rcode'] = 200;
+
+				} else {
+					$data['msg'] = 'Unavaliable Token.';	
+				}
+
+			} else {
+				$data['msg'] = 'Token is missing.';
+			}
+			
+		} else {
+			$data['msg'] = 'Missing parameter.';
+		}
+
+		echo json_encode($data);
+	}
+
+	public function handdown( $cid=0 ) {
+
+		$cid = (int)$cid;
+		$data = array( 'rcode' => 500, 'msg' => '' );
+
+		$token = $this->input->post('token');
+
+		if( $cid != 0 ) {
+
+			if( $token != null ) {
+
+				$this->load->model('token');
+				$user = $this->token->tokenAuth( $token );
+
+				if( $user != false ) {
+
+					// 確定舉手狀態
+					$hand = $this->db->get_where( 'handup', array( 'cid'=>$cid, 'uid'=>$user->uid ) );
+					if( $hand->num_rows == 1 ) {
+						
+						$hand = $hand->result()[0];
+						if( $hand->token != '' ) {
+							// 已經舉
+							// 送出清除用訊號
+							$this->db->insert( 'hand_queue', array( 'cid'=>$cid, 'token'=>$hand->token, 'time'=>time(), 'command'=>'quit' ) );
+						}
+
+						// 刪除手
+						$this->db->delete( 'handup', array( 'cid'=>$cid, 'uid'=>$user->uid ) );
+						$data['rcode'] = 200;
+
+					} else {
+						// 老師可能拒絕舉手
+						$data['rcode'] = 404;
+						$data['msg'] = 'No Hand';
+					}
+					
+
+				} else {
+					$data['msg'] = 'Unavaliable Token.';	
+				}
+
+			} else {
+				$data['msg'] = 'Token is missing.';
+			}
+			
+		} else {
+			$data['msg'] = 'Missing parameter.';
+		}
+
+		echo json_encode($data);
+	}
+
 }
 
 /* EOF */
